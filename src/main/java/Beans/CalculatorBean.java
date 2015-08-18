584833b786f899b37012ec6c2eb3634b747d1ec4
unknown <ashique600@gmail.com>
2015-08-18 07:22:12 +0200

    functional operation list added

--- src/main/java/Beans/CalculatorBean.java
+++ src/main/java/Beans/CalculatorBean.java
@@ -19,10 +19,11 @@ import my.calc.Calculation;
 public class CalculatorBean implements Serializable {
     private static double result = 0;
     private static int counter = 0;
-    public static String operators = ""; // should not use static.... {issue}
     
-    private String text="";
-
+    private static boolean plusClicked = false, minusClicked = false, mulClicked = false, divClicked = false;
+    
+    private String text="0";
+    private String msg="";
     
     public String getText() {
         return text;
@@ -31,102 +32,133 @@ public class CalculatorBean implements Serializable {
     public void setText(String text) {
         this.text = text;
     }
-    
 
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+    
+    
     //Button number click events
     /*===========================*/
     
     public void clickedZero() throws Exception {
        text = text + "0";
+       msg = msg + "0";
     }
     
     public void clickedOne() throws Exception {
        text = text + "1";
+       msg = msg + "1";  
     }
     
     public void clickedTwo() throws Exception {
        text = text + "2";
+       msg = msg + "2";
     }
     
     public void clickedThree() throws Exception {
        text = text + "3";
+       msg = msg + "3";
     }
     
     public void clickedFour() throws Exception {
        text = text + "4";
+       msg = msg + "4";
     }
     
     public void clickedFive() throws Exception {
        text = text + "5";
+       msg = msg + "5";
     }
       
     public void clickedSix() throws Exception {
        text = text + "6";
+       msg = msg + "6";
     }
        
     public void clickedSeven() throws Exception {
        text = text + "7";
+       msg = msg + "7";
     }
         
     public void clickedEight() throws Exception {
        text = text + "8";
+       msg = msg + "8";
     }
          
     public void clickedNine() throws Exception {
        text = text + "9";
+       msg = msg + "9";
     }
     
     public void clickedPoint() throws Exception {
-        if(!text.contains("."))
+        if(!text.contains(".")) {
              text = text + ".";
+             msg = msg + ".";
+        }else 
+            msg = msg + "Point cannot be inserted twice in a number\n";
     }
     
     //Operation Buttons
     /*==================*/
     
-    public void clickedPlus() {
+    public void clickedPlus() throws Exception {
+        msg = msg + " + ";
         counter++;
-        if(counter==1)
+        if(counter<=1)
             result = Double.parseDouble(text);
         else {
             result = result + Double.parseDouble(text);
         }
         text = "";
-        operators = "+";
         
+        plusClicked = true;
+        minusClicked = mulClicked = divClicked = false;
     }
     
-    public void clickedMinus() {
+    //some issues have here
+    public void clickedMinus() throws Exception {
+        msg = msg + " - ";
         counter++;
-        if(counter==1) {
-            result = Double.parseDouble(text);
+        if(counter<=1) {
+            result = Double.parseDouble(text) + 0;
         }else {
             result = result -  Double.parseDouble(text);
         }
         text = "";
-        operators = "-";
+        
+        minusClicked = true;
+        plusClicked = mulClicked = divClicked = false;
     }
     
-    public void clickedMul() {
+    public void clickedMul() throws Exception {
+        msg = msg + " * ";
         counter++;
-        if(counter==1)
-            result= Double.parseDouble(text);
-        else {
+        if(counter<=1){
+                result= Double.parseDouble(text);
+        }else {
             result = result * Double.parseDouble(text);
         }
         text = "";
-        operators = "*";
+        mulClicked = true;
+        minusClicked = plusClicked = divClicked = false;
     }
     
-    public void clickedDiv() {
+    public void clickedDiv() throws Exception {
+        msg = msg + " / ";
         counter++;
-        if(counter==1)
+        if(counter<=1)
             result = Double.parseDouble(text);
         else{
             result = result / Double.parseDouble(text);
         }
         text = "";
-        operators = "/";
+        divClicked = true;
+        minusClicked = mulClicked = plusClicked = false;
     }
     
     public void plusMinus() {
@@ -134,29 +166,32 @@ public class CalculatorBean implements Serializable {
     }
     
     public void screenClear() throws Exception {
+        msg = "";
         text = "0";
         result = 0;
-        operators = "";
+        plusClicked = minusClicked = mulClicked = divClicked = false;
         counter = 0;
     }
     
     public void clickEquals() throws Exception {
         counter++;
-        if(operators.equals("+")){
+        if(plusClicked){
             text = Double.toString(Calculation.getInstance().addition(result, Double.parseDouble(text)));
+            msg = msg + "; output is: "+text+"\n";
             counter=0;
-        }
-        if(operators.equals("-")){
+        } else if(minusClicked){
             text = Double.toString(Calculation.getInstance().subtraction(result, Double.parseDouble(text)));
+            msg = msg + "; output is: "+text+"\n";
             counter = 0;
-        }
-        if(operators.equals("*")){
+        }else if(mulClicked){
             text = Double.toString(Calculation.getInstance().multiplication(result, Double.parseDouble(text)));
+            msg = msg + "; output is: "+text+"\n";
             counter = 0;
-        }
-        if(operators.equals("/")){
+        }else if(divClicked){
             text = Double.toString(Calculation.getInstance().division(result, Double.parseDouble(text)));
+            msg = msg + "; output is: "+text+"\n";
             counter = 0;
-        } 
+        }else
+            msg = msg + "\nOperation Invalid. Please try again.\n";
     }    
 }
--- src/main/webapp/action.js
+++ src/main/webapp/action.js
@@ -3,22 +3,31 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
-$(document).ready(function() {
-    var posX = 0, posY = 0, time=0;
-     var timer = function() { //showing the moving bar after 3-4 seconds
-        var display = setInterval(function() {
-            time ++;
-            if(time > Math.random()*4+3)
-                $("#myJS").css("visibility","visible");
-            //clearInterval(display);
-        },1000);
-    };
-    timer();
+ 
+var timer = function() { //showing the moving bar after 3-4 seconds
+    time=0;
+    var display = setInterval(function() {
+        time ++;
+        if(time > Math.random()*4+3)
+            $("#myJS").css("visibility","visible");
+        //clearInterval(display);
+    },1000);
+};
+timer();
+    
+var movingBar = function() { // moving bar on the chart
+    var posX = 0;
     setInterval(function() {
         posX = Math.random()*450+400;
+        //posY = (Math.random()*(50)+(10));
         document.getElementById("myJS").style.left = posX+"px";
+        //document.getElementById("myJS").style.bottom = posY+"px";
         //document.getElementById("myJS").style.top = posY+"px";
     },Math.random()*700+500);
-});
+    
+    $("#hideBar").click(function() {
+        $("#myJS").hide();
+    });
+};
+movingBar();
 
